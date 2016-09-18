1cc782d8a69a0cfaebbb384efc4ba764f3ccc72a
zhihuawang <zhihuawang@192.168.1.84>
2016-09-19 01:53:52 +0800

    commit

--- src/main/java/me/gacl/action/TestAction.java
+++ src/main/java/me/gacl/action/TestAction.java
@@ -7,9 +7,10 @@ import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@ParentPackage("basePackage")
+/*@ParentPackage("basePackage")
 @Action(value="strust2Test")//使用convention-plugin插件提供的@Action注解将一个普通java类标注为一个可以处理用户请求的Action，Action的名字为struts2Test
 @Namespace("/")//使用convention-plugin插件提供的@Namespace注解为这个Action指定一个命名空间
+*/
 public class TestAction {
     
     /**
@@ -27,5 +28,11 @@ public class TestAction {
     public void test(){
         System.out.println("进入TestAction");
         userService.test();
+    }
+    
+    public String zhangsan(){
+        System.out.println("进入zhangsan");
+        userService.test();
+        return "success";
     }
 }
